47f6b0284fda4e0e18ab3f7ad357f286db905432
SelimHorri <horri.slim@gmail.com>
2021-11-22 21:02:18 +0100

    setup all configs with eureka

--- shipping-service/src/main/java/com/selimhorri/app/ShippingServiceApplication.java
+++ shipping-service/src/main/java/com/selimhorri/app/ShippingServiceApplication.java
@@ -2,12 +2,34 @@ package com.selimhorri.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ShippingServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShippingServiceApplication.class, args);
 	}
-
+	
+	
+	
 }
+
+@RestController
+class ShippingController {
+	
+	@GetMapping
+	public String msg() {
+		return "shipping controller responding!!";
+	}
+	
+}
+
+
+
+
+
+
--- user-service/src/main/java/com/selimhorri/app/UserServiceApplication.java
+++ user-service/src/main/java/com/selimhorri/app/UserServiceApplication.java
@@ -2,12 +2,34 @@ package com.selimhorri.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
-
+	
+	
+	
 }
+
+@RestController
+class UserController {
+	
+	@GetMapping
+	public String msg() {
+		return "User controller responding!!";
+	}
+	
+}
+
+
+
+
+
+

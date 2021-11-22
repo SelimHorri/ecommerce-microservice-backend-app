920373c9e048b48f77125ece4b924e69515b4fed
SelimHorri <horri.slim@gmail.com>
2021-11-22 19:28:31 +0100

    remove default test case

--- order-service/src/test/java/com/selimhorri/app/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/selimhorri/app/OrderServiceApplicationTests.java
@@ -1,13 +1,16 @@
 package com.selimhorri.app;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OrderServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+	
+	
+	
 }
+
+
+
+
+
+

1823c19d939830e1284ab817fdef2a2f9d028af6
SelimHorri <horri.slim@gmail.com>
2021-11-22 22:38:45 +0100

    add gateway configs and adjust others

--- api-gateway/src/test/java/com/selimhorri/app/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/selimhorri/app/ApiGatewayApplicationTests.java
@@ -1,13 +1,17 @@
 package com.selimhorri.app;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApiGatewayApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+	
+	
+	
 }
+
+
+
+
+
+
+

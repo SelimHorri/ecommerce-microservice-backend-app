53884ba72764258c7f2816320dbdddf25fb6a8c6
SelimHorri <horri.slim@gmail.com>
2021-11-22 19:24:35 +0100

    create domain microservices

---
+++ payment-service/src/test/java/com/selimhorri/app/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selimhorri.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ service-discovery/src/main/java/com/selimhorri/app/ServiceDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.selimhorri.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDiscoveryApplication.class, args);
+	}
+
+}

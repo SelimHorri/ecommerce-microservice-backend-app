736728c19655ffabd0c110b227f86692a88124bc
SelimHorri <horri.slim@gmail.com>
2021-11-23 12:18:13 +0100

    remove default test case from config server

--- cloud-config/src/test/java/com/selimhorri/app/CloudConfigApplicationTests.java
+++ cloud-config/src/test/java/com/selimhorri/app/CloudConfigApplicationTests.java
@@ -1,13 +1,16 @@
 package com.selimhorri.app;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CloudConfigApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+	
+	
+	
 }
+
+
+
+
+
+

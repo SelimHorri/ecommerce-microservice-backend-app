b2264d69f997cd2bae4e9d1d97b2a7a9dd5cd6f2
SelimHorri <horri.slim@gmail.com>
2021-11-30 17:10:26 +0100

    add fetch username method in both repos

--- user-service/src/main/java/com/selimhorri/app/repository/UserRepository.java
+++ user-service/src/main/java/com/selimhorri/app/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package com.selimhorri.app.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.selimhorri.app.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 	
-	
+	Optional<User> findByCredentialUsername(final String username);
 	
 }

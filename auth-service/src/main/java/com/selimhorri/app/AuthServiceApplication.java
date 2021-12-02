88141b013dea3173b574e022ae479e27422e5dd2
SelimHorri <horri.slim@gmail.com>
2021-12-02 18:40:30 +0100

    init auth-service

---
+++ auth-service/src/main/java/com/selimhorri/app/AuthServiceApplication.java
@@ -0,0 +1,27 @@
+package com.selimhorri.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class AuthServiceApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceApplication.class, args);
+	}
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
---
+++ auth-service/src/test/java/com/selimhorri/app/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selimhorri.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

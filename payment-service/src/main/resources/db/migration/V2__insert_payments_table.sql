6a722f6ad97c9c67bf5e785d265ade2de3bbeaf0
SelimHorri <horri.slim@gmail.com>
2021-12-14 20:31:00 +0100

    add payment business layer

--- payment-service/src/main/resources/db/migration/V2__insert_payments_table.sql
+++ payment-service/src/main/resources/db/migration/V2__insert_payments_table.sql
@@ -1,8 +1,8 @@
 
 INSERT INTO payments
 (order_id, is_payed, payment_status) VALUES
-(1, false, 'on_hold'),
-(2, false, 'on_hold'),
-(3, false, 'on_hold'),
-(4, false, 'on_hold');
+(1, false, 'IN_PROGRESS'),
+(2, false, 'IN_PROGRESS'),
+(3, false, 'IN_PROGRESS'),
+(4, false, 'IN_PROGRESS');
 

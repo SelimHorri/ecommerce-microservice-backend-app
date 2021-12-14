a5baa8bf92d0edd64afc5d3b89620ccf83822f59
SelimHorri <horri.slim@gmail.com>
2021-12-14 18:38:24 +0100

    setup data layer

---
+++ payment-service/src/main/java/com/selimhorri/app/dto/PaymentDto.java
@@ -0,0 +1,39 @@
+package com.selimhorri.app.dto;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.selimhorri.app.domain.PaymentStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class PaymentDto implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Integer paymentId;
+	private Boolean isPayed;
+	private PaymentStatus paymentStatus;
+	
+	@JsonInclude(Include.NON_NULL)
+	private OrderDto orderDto;
+	
+}
+
+
+
+
+
+
+
+
+
+
